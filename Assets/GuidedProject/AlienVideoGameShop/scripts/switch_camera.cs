ee67b82b8361144ee0e0ff1ac5ebb8306f39720c
zaidan126 <134616360+zaidan126@users.noreply.github.com>
2023-12-28 19:41:22 +0700

    Initial commit

---
+++ Assets/GuidedProject/AlienVideoGameShop/scripts/switch_camera.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using UnityEditor.VersionControl;
+using UnityEngine;
+
+public class switch_camera : MonoBehaviour
+{
+    public Camera wide;
+    public Camera character;
+    public Camera closeup;
+    public Camera window;
+    // Start is called before the first frame update
+    void Start()
+    {
+     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            wide.enabled = true;
+            character.enabled = false;
+            closeup.enabled = false;
+            window.enabled = false;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            wide.enabled = false;
+            character.enabled = true;
+            closeup.enabled = false;
+            window.enabled = false;
+        }
+       else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            wide.enabled = false;
+            character.enabled = false;
+            closeup.enabled = true;
+            window.enabled = false;
+        }
+       else if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            wide.enabled = false;
+            character.enabled = false;
+            closeup.enabled = false;
+            window.enabled = true;
+        }
+    }
+}
